792ec01f479444b6c073a14a4b57f2145855775f
Moreno Yassine <ymoreno@if501-219-12.insa-lyon.fr>
2015-03-17 11:52:05 +0100

    subtileParse

--- LTN/src/File.cpp
+++ LTN/src/File.cpp
@@ -67,9 +67,9 @@ vector<vector<string> > File::getParsedFiles()
 
 void File::ParsingTest()
 {
-	for (int i = 0;i<parsedFiles.size();i++)
+	for (unsigned int i = 0;i<parsedFiles.size();i++)
 	{
-		for (int j=0;j<parsedFiles[i].size();j++)
+		for (unsigned int j=0;j<parsedFiles[i].size();j++)
 		{
 			cout << parsedFiles[i][j] << endl;
 		}
