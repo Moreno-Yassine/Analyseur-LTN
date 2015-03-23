efb7b9f31d4f68cd8e9cd5d96d3b7f22fbf9f748
Yassine Moreno <slifer.unit@gmail.com>
2015-03-23 17:17:29 +0100

    file master

--- LTN/src/File.cpp
+++ LTN/src/File.cpp
@@ -3,6 +3,11 @@
 using namespace std;
 
 const string tok = ";";
+const string vars = "var";
+const string consts = "const";
+const string minitok = ",";
+const string equals = "=";
+const string affct = ":=";
 
 File::File()
 {
@@ -16,7 +21,7 @@ void File::setPath(string link)
 
 File::~File()
 {
-    //dtor
+
 }
 
 void File::openParse()
@@ -37,21 +42,53 @@ void File::openParse()
                 string temp;
                 while (ss >> temp)
                 {
-					if (temp.find(tok) == string::npos)
-					{
-						array.push_back(temp);
-					}
-					else
-					{
-						array.push_back(temp.substr(0,temp.find(tok)));
-						array.push_back(tok);
-					}
+                	// Tant que le mot n'a pas été consommé entièrement
+                	while (temp != "")
+                	{
+                		// On a détecté :=
+	                	if (temp.find(affct) != string::npos)
+	                	{
+	                		array.push_back(temp.substr(0,temp.find(affct)));
+	                		array.push_back(affct);
+	                		size_t sub = temp.find(affct)+2;
+	                		temp = temp.substr(sub);
+	                	}
+	                	// On a détecté ,
+	                	if (temp.find(minitok) != string::npos)
+	                	{
+	                		array.push_back(temp.substr(0,temp.find(minitok)));
+	                		array.push_back(minitok);
+	                		size_t sub = temp.find(minitok)+1;
+	                		temp = temp.substr(sub);
+	                	}
+						// On a détecté =
+						if (temp.find(equals) != string::npos)
+	                	{
+	                		array.push_back(temp.substr(0,temp.find(equals)));
+	                		array.push_back(equals);
+	                		size_t sub = temp.find(equals)+1;
+	                		temp = temp.substr(sub);
+	                	}
+
+	                	// On a détecté ;
+						if (temp.find(tok) == string::npos)
+						{
+							array.push_back(temp);
+							temp = "";
+						}
+						else
+						{
+							array.push_back(temp.substr(0,temp.find(tok)));
+							array.push_back(tok);
+							temp = "";
+						}
+                	}
 				}
                 parsedFiles.push_back(array);
 			}                   
         } 
 	    myfile.close();
-	    ParsingTest();
+		cleaning();
 	  }
 	else
 	{
@@ -83,7 +120,22 @@ void File::ParsingTest()
 	{
 		for (unsigned int j=0;j<parsedFiles[i].size();j++)
 		{
+			if (parsedFiles[i][j] != "")
 			cout << parsedFiles[i][j] << endl;
+			else
+			cout << "NULL" << endl;
+		}
+	}
+}
+
+void File::cleaning()
+{
+	for (unsigned int i = 0;i<parsedFiles.size();i++)
+	{
+		for (unsigned int j=0;j<parsedFiles[i].size();j++)
+		{
+			if (parsedFiles[i][j] == "")
+			parsedFiles[i].erase(parsedFiles[i].begin()+j);
 		}
 	}
 }
